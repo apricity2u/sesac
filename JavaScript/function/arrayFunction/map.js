7058f802cabf048cf6b09278abcbc9f6b7da13ab
apricity2u <gkdms9849@gmail.com>
2024-10-24 11:24:50 +0900

    modify map.js

--- JavaScript/function/arrayFunction/map.js
+++ JavaScript/function/arrayFunction/map.js
@@ -1,36 +1,39 @@
 const numbers = [1, 2, 3, 4];
 
-const doubled = numbers.map( // doubled 라는 함수는 
-                            // numbers라는 array에 괄호 안에 있는 함수를 적용할거야
-    (num) => {
+const doubled = numbers.map(
+  // doubled 라는 함수는
+  // numbers라는 array에 괄호 안에 있는 함수를 적용할거야
+  (num) => {
     return num * 2;
     // numbers라는 array에 있는 요소들을 매개변수 num에 넣을거고.
     // 2를 곱한 값을 반환할거야
-}
+  }
 );
 
 console.log(doubled); // [2, 4, 6, 8]
 
 // arr = [x, y, z] 배열에
-// new_arr = [f(x), f(y), f(z)] 함수를 적용하는 것이 바로 map 
+// new_arr = [f(x), f(y), f(z)]
+// 함수가 적용된 새로운 array를 만드는 것이 바로 map
 
-
-const strNumbers = ['1', '2', '3', '4'] 
+const strNumbers = ['1', '2', '3', '4'];
 // string 형태로 된 숫자
-console.log(parseInt('1')) // 1
-console.log('1') // '1'
+console.log(parseInt('1')); // 1
+console.log('1'); // '1'
 
-const intNumbers = [1, 2, 3, 4] 
+const intNumbers = [1, 2, 3, 4];
 // 숫자형 형태로 된 숫자
 
-const changeType = strNumbers.map (
-    // changeType이라는 함수는 strNumbers라는 array에 
-    // 괄호 안에 있는 기능을 사용할거야 
-    (num) => {
-        return parseInt(num)
-    }
-    // num 이라는 매개변수에 array에 있는 요소들을
-    // 인자로 받아서 return 할거야 
-)
+const changeType = strNumbers.map(
+  // changeType이라는 함수는 strNumbers라는 array에
+  // 괄호 안에 있는 기능을 사용할거야
+  (num) => {
+    // {} 와 return을 생략해도 괜찮음
+    return parseInt(num);
+  }
 
-console.log(changeType)
+  // num 이라는 매개변수에 array에 있는 요소들을
+  // 인자로 받아서 return 할거야
+);
+
+console.log(changeType); // [ 1, 2, 3, 4 ]
