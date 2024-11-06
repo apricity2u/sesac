046215dda8759042c97a7334941a5891732302f8
apricity2u <gkdms9849@gmail.com>
2024-11-06 10:57:00 +0900

    component in component

---
+++ React/props/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Container({children}) {
+  return (
+    <div style={{
+      width: 300,
+      height: 300,
+      border: '1px solid black',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems : 'center'
+    }}>{children}</div>
+  )
+}
--- React/props/Welcome.jsx
+++ React/props/Welcome.jsx
@@ -1,3 +1,3 @@
-export default function Welcome({ name }) {
-  return <div>안녕하세요, {name}님!</div>;
+export default function Welcome({ children }) {
+  return <div>안녕하세요, {children}님!</div>;
 }
