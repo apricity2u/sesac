874957ab1b733f8a9fc643b51c0eb75c2a7cf5fa
apricity2u <gkdms9849@gmail.com>
2024-11-08 11:49:13 +0900

    structure homepage with HTML

---
+++ React/list/apparel/Homepage.jsx
@@ -0,0 +1,86 @@
+export default function Homepage() {
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>
+            <a href="">APPAREL SHOP</a>
+          </h1>
+          <ul>
+            <li>
+              <a href="">HOME</a>
+            </li>
+            <li>
+              <a href="">PRODUCTS</a>
+            </li>
+          </ul>
+        </nav>
+        <img src="" alt="" />
+      </header>
+
+      <main>
+        <section>
+          <img src="" alt="" />
+          <div>
+            <h2>About Our Company</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+              minima maxime quidem tempore nobis sint aspernatur. Nobis officia
+              corrupti facere recusandae quis amet delectus autem iusto,
+              deserunt molestias praesentium aliquam.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <h2>Contact Us</h2>
+          <div>
+            <form action="">
+              <label htmlFor="name">Username</label>
+              <input type="text" name="name" id="name" placeholder="Username" />
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" placeholder="Email"/>
+              <input type="submit" value="Send"/>
+            </form>
+            <img src="" alt="" />
+          </div>
+        </section>
+
+        <section>
+          <h2>More</h2>
+          <ul>
+            <li>
+              <div>
+                <img src="" alt="" />
+                <div>Email Address</div>
+              </div>
+              <div>dksjflsfk@naver.com</div>
+            </li>
+            <li>
+              <div>
+                <img src="" alt="" />
+                <div>Email Address</div>
+              </div>
+              <div>dksjflsfk@naver.com</div>
+            </li>
+            <li>
+              <div>
+                <img src="" alt="" />
+                <div>Email Address</div>
+              </div>
+              <div>dksjflsfk@naver.com</div>
+            </li>
+            <li>
+              <div>
+                <img src="" alt="" />
+                <div>Email Address</div>
+              </div>
+              <div>dksjflsfk@naver.com</div>
+            </li>
+          </ul>
+        </section>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
