b321ada07308f8a22c5cf514e2c3c270e9b407f3
apricity2u <gkdms9849@gmail.com>
2024-11-17 21:00:07 +0900

    [feat] render Menu Board ver.2
    데이터를 object로 관리하고, key : value를 각각 category : [데이터] 형태로 만듦

---
+++ React/list/menubetter/MenuCategoryItem.jsx
@@ -0,0 +1,8 @@
+export default function MenuCategoryItem({ name, price }) {
+  return (
+    <li className="flex justify-between">
+      <div>{name}</div>
+      <div>{price}</div>
+    </li>
+  );
+}
