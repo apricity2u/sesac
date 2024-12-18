b92d13717e061efc68f4c1791e577cf3e81e4297
apricity2u <gkdms9849@gmail.com>
2024-12-18 14:09:51 +0900

    supplement interface

--- Java/practice/src/main/java/org/example/interfaceprac2/Character.java
+++ Java/practice/src/main/java/org/example/interfaceprac2/Character.java
@@ -12,6 +12,11 @@ public abstract class Character {
         this.level = 1;
     }
 
+    // implement DamageTakable
+    // public abstract void attack(DamageTakable otherSide);
+    // 타입에 클래스 뿐 아니라 인터페이스도 들어갈 수 있다.
+    // 해당 인터페이스를 물려받은 것들은 다 들어갈 수 있다.
+    // 물론 이런 경우가 많아지면 추상 클래스로 만드는게 나을 수도 있다.
     public abstract void attack(Character otherSide);
 
     public abstract void takeDamage();
