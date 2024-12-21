55604ee2f3a859a49d760a32e50045d56408cbbf
apricity2u <gkdms9849@gmail.com>
2024-12-21 23:50:32 +0900

    supplement composition prac

--- Java/practice/src/main/java/org/example/composition/Weapon.java
+++ Java/practice/src/main/java/org/example/composition/Weapon.java
@@ -10,5 +10,9 @@ public abstract class Weapon implements SpecialAttackable {
         this.durability = durability;
     }
 
+    // 1. Weapon이 SpecialAttackable을 기본 null 값으로 가지고, 스페셜 공격을 하는 클래스만 override
+    // 2. Weapon이 SpecialAttackable을 composition으로 가지기
+
+    // 과연 SpecialAttackable 인터페이스를 따로 만드는게 맞았을까?
 
 }
