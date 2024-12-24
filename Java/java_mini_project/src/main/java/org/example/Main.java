9d506aa1316c1e0e391b92021c3f38a3834d50ee
apricity2u <gkdms9849@gmail.com>
2024-12-24 11:05:49 +0900

    [chore] make instance with VendingMachine, Product class

--- Java/java_mini_project/src/main/java/org/example/Main.java
+++ Java/java_mini_project/src/main/java/org/example/Main.java
@@ -3,6 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        // 자판기 생성
+        VendingMachine vendingMachine = new VendingMachine();
+
+        // 상품 생성
+        Product cola = new Beverages("cola", 1000, 5);
+        Product cider = new Beverages("cider", 800, 8);
+        Product chocolate = new Snack("chocolate", 1500, 2);
+        Product candy = new Snack("candy", 300, 10);
+
+
+
     }
 
 }
