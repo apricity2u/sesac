6852d38ac685ab7f1a84faebb810678e95544e4c
apricity2u <gkdms9849@gmail.com>
2024-12-24 10:08:54 +0900

    [feat] make field and constructor in Product class

--- Java/java_mini_project/src/main/java/org/example/Product.java
+++ Java/java_mini_project/src/main/java/org/example/Product.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public abstract class Product {
+
+    private String name;
+    private int price;
+    private int stock;
+
+    public Product(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
 }
