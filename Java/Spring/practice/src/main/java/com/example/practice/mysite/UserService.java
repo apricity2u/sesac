bb36defe1bb63f0203dcf0a009f1d7a7c9bdc319
apricity2u <gkdms9849@gmail.com>
2025-01-06 13:45:50 +0900

    add DI in controller and service

--- Java/Spring/practice/src/main/java/com/example/practice/mysite/UserService.java
+++ Java/Spring/practice/src/main/java/com/example/practice/mysite/UserService.java
@@ -1,7 +1,13 @@
 package com.example.practice.mysite;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
 }
