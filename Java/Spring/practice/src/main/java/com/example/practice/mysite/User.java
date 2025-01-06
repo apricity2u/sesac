97866c8a7acb65743a76301e0e8b3e3ec79abbb4
apricity2u <gkdms9849@gmail.com>
2025-01-06 13:38:11 +0900

    add column in Entity

--- Java/Spring/practice/src/main/java/com/example/practice/mysite/User.java
+++ Java/Spring/practice/src/main/java/com/example/practice/mysite/User.java
@@ -1,12 +1,27 @@
 package com.example.practice.mysite;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, updatable = false, nullable = false)
     private String username;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
     private String nickname;
     private Integer age;
     private Boolean isActive;
