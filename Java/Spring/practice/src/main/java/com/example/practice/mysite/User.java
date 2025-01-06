c9ce7818cdc217e8742fd5e53c656297eb6104da
apricity2u <gkdms9849@gmail.com>
2025-01-06 11:21:37 +0900

    make User class and field in class

---
+++ Java/Spring/practice/src/main/java/com/example/practice/mysite/User.java
@@ -0,0 +1,16 @@
+package com.example.practice.mysite;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String nickname;
+    private Integer age;
+    private Boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
