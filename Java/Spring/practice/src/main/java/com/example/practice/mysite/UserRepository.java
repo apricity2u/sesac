6902659e332214c25fa77403dba3850bb198aab4
apricity2u <gkdms9849@gmail.com>
2025-01-06 11:37:27 +0900

    use Spring Data JPA in repository

--- Java/Spring/practice/src/main/java/com/example/practice/mysite/UserRepository.java
+++ Java/Spring/practice/src/main/java/com/example/practice/mysite/UserRepository.java
@@ -1,7 +1,8 @@
 package com.example.practice.mysite;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
